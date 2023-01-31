dfcc45721166d48599745593ae41019bac15bcae
Yelzhan Ayazbekov <yelzhan.ayazbekov@samgau.com>
2023-01-31 16:13:56 +0600

    my solution for 392. Is Subsequence(O(n))

---
+++ src/com/lg/algo/leetcode/easy/IsSubsequence.java
@@ -0,0 +1,34 @@
+package com.lg.algo.leetcode.easy;
+
+//392. Is Subsequence
+//https://leetcode.com/problems/is-subsequence
+public class IsSubsequence {
+
+    public static void main(String[] args) {
+        IsSubsequence isSubsequence = new IsSubsequence();
+
+        String s = "abc", t = "ahbgdc";
+        System.out.println(isSubsequence.isSubsequence(s, t));
+    }
+
+
+    public boolean isSubsequence(String s, String t) {
+        if (s.length() == 0) return true;
+
+        String word = "";
+
+        for (int i = 0, index = 1; i < t.length(); i++) {
+            word += t.charAt(i);
+
+            if (s.substring(0, index).equals(word)) {
+                index++;
+            } else {
+                word = word.substring(0, word.length() - 1);
+            }
+
+            if (s.equals(word)) return true;
+        }
+
+        return false;
+    }
+}
